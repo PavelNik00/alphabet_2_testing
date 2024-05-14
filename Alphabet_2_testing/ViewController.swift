8120750a8eea94cd318dfe99641896e187978efa
PavelNik00 <gh9.111@yandex.ru>
2024-05-14 13:16:14 +0200

    Initial Commit

---
+++ Alphabet_2_testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alphabet_2_testing
+//
+//  Created by Pavel Nikipelov on 14.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
