04f4971eec850fba66c21f29f069b2ed33a3b6bc
PavelNik00 <gh9.111@yandex.ru>
2024-05-14 23:15:24 +0200

    Добавлен массив с буквами. Настроены параметры показа букв в ячейке.

--- Alphabet_2_testing/ViewController.swift
+++ Alphabet_2_testing/ViewController.swift
@@ -18,6 +18,12 @@ final class ViewController: UIViewController {
         return collectionView
     }()
     
+    private let letters = [
+                "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к",
+                "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц",
+                "ч", "ш" , "щ", "ъ", "ы", "ь", "э", "ю", "я"
+            ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
               
@@ -39,8 +45,7 @@ final class ViewController: UIViewController {
         ])
         
         collectionView.dataSource = self
-//        collectionView.delegate = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.delegate = self
 
     }
 
@@ -49,14 +54,22 @@ final class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 130
+        return letters.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.contentView.backgroundColor = .blue
-        cell.contentView.alpha = 0.5
-        return cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? LetterCollectionViewCell
+        
+        cell?.titleLabel.text = letters[indexPath.row]
+        cell?.titleLabel.textColor = .white
+        cell?.titleLabel.font = .systemFont(ofSize: 24)
+        cell?.titleLabel.textAlignment = .center
+        cell?.titleLabel.numberOfLines = 1
+
+        cell?.contentView.backgroundColor = .blue
+        cell?.contentView.alpha = 0.5
+        
+        return cell!
     }
 }
 
